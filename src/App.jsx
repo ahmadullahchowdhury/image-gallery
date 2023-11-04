4cd497bf508d4cae2a5f3398452cbd29ee554dd7
ahmadullahchowdhury <kowcher99@gmail.com>
2023-11-04 16:50:56 +0600

    dnd kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,210 @@
+// import { useState } from "react";
+// import "./App.css";
+// import { Images } from "./util/image.util";
+
+// function App() {
+//   const [images, setImages] = useState([...Images]);
+
+//   const [draggedImage, setDraggedImage] = useState(null);
+
+//   const handleUnChecked = () => {
+//     const uncheckedImages = images.map((img) => {
+//       return { ...img, isChecked: false };
+//     });
+//     setImages(uncheckedImages);
+//   };
+
+//   const checked = (index) => {
+//     const updatedImages = [...images];
+//     updatedImages[index].isChecked = !updatedImages[index].isChecked;
+//     setImages(updatedImages);
+//   };
+
+//   const handleDelete = () => {
+//     console.log("delete");
+//     const remainingImages = images.filter((image) => !image.isChecked);
+//     setImages(remainingImages);
+//   };
+
+//   const handleDragStart = (image) => {
+//     setDraggedImage(image);
+//   };
+
+//   const handleDrop = (targetIndex) => {
+//     if (draggedImage) {
+//       const updatedImages = [...images];
+
+//       const removedImageIndex = updatedImages.findIndex(
+//         (image) => image.id === draggedImage.id
+//       );
+//       if (removedImageIndex !== -1) {
+//         updatedImages.splice(removedImageIndex, 1);
+//         updatedImages.splice(targetIndex, 0, draggedImage);
+//         setImages(updatedImages);
+//         setDraggedImage(null);
+//       }
+//     }
+//   };
+
+//   const totalChecked = images.filter((image) => image.isChecked).length ?? 0;
+
+//   const ImageCard = ({ elm, index }) => {
+//     return (
+//       <div
+//         draggable={true}
+//         onDragStart={() => handleDragStart(elm)}
+//         onDrop={() => handleDrop(index)}
+//         className={`${index === 0 ? "row-span-2 col-span-2" : ""} group `}
+//         key={index}
+//         onClick={() => checked(index)}
+//       >
+//         <div className="relative overflow-hidden rounded-lg border-slate border-2  ">
+//           <img
+//             className="w-full"
+//             data-id={elm.id}
+//             alt={elm.id}
+//             src={elm?.image}
+//           />
+//           <div
+//             className={`absolute h-full w-full ${
+//               elm?.isChecked
+//                 ? "bottom-0 opacity-100 bg-black/20"
+//                 : "bg-black/40 opacity-0 -bottom-10"
+//             }  group-hover:bottom-0 group-hover:opacity-100 ease-out  group-hover:ease-out delay-150 duration-300`}
+//           >
+//             <div className=" py-5 px-5">
+//               <input
+//                 className="w-5 h-5"
+//                 // onChange={() => checked(index)}
+//                 checked={elm?.isChecked}
+//                 type="checkbox"
+//               ></input>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   return (
+//     <>
+//       <div className="bg-white rounded-xl   ">
+//         <div className="px-1.5 py-3 border-black border-b-2">
+//           {totalChecked ? (
+//             <div className="flex flex-row items-center justify-between  ">
+//               <div className="flex gap-4 border-white border-4">
+//                 <input
+//                   className="mt-1 h-5 w-5 md:h-7 md:w-7"
+//                   type="checkbox"
+//                   onChange={() => handleUnChecked()}
+//                   checked={totalChecked > 0 ? true : false}
+//                 />
+//                 <span className="text-base md:text-2xl font-semibold">
+//                   {totalChecked} Images Selected
+//                 </span>
+//               </div>
+
+//               <div
+//                 className=" text-base md:text-2xl text-red-600 bg-slate-200 hover:bg-slate-400 rounded-md hover:cursor-pointer font-semibold px-2 md:px-4 py-1 md:py-2"
+//                 onClick={handleDelete}
+//               >
+//                 Delete
+//               </div>
+//             </div>
+//           ) : (
+//             <div className="text-base md:text-2xl py-2 font-semibold">
+//               Image Gallery
+//             </div>
+//           )}
+//         </div>
+
+//         <div
+//           onDragOver={(e) => {
+//             e.preventDefault();
+//           }}
+//           className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 pt-5 "
+//         >
+//           {images.length > 0 ? (
+//             images.map((elm, index) => (
+//               <ImageCard key={index} index={index} elm={elm} />
+//             ))
+//           ) : (
+//             <p className="text-3xl font-bold">No Images </p>
+//           )}
+//           <div className="flex flex-col w-full items-center justify-center bg-slate-100 border-dashed border-black border-2 rounded-lg">
+//             <svg
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               strokeWidth="1.5"
+//               stroke="currentColor"
+//               className="w-5 h-5"
+//             >
+//               <path
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+//               />
+//             </svg>
+
+//             <p>Upload Images</p>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
 import { useState } from "react";
 import "./App.css";
 import { Images } from "./util/image.util";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { SortableContext, arrayMove, rectSortingStrategy, useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function App() {
   const [images, setImages] = useState([...Images]);
 
+  const [dragging, setDragging] = useState(false);
   const [draggedImage, setDraggedImage] = useState(null);
-
-  const handleUnChecked = () => {
-    const uncheckedImages = images.map((img) => {
-      return { ...img, isChecked: false };
-    });
-    setImages(uncheckedImages);
-  };
-
-  const checked = (index) => {
-    const updatedImages = [...images];
-    updatedImages[index].isChecked = !updatedImages[index].isChecked;
-    setImages(updatedImages);
-  };
-
-  const handleDelete = () => {
-    console.log("delete");
-    const remainingImages = images.filter((image) => !image.isChecked);
-    setImages(remainingImages);
-  };
-
-  const handleDragStart = (image) => {
-    setDraggedImage(image);
-  };
-
-  const handleDrop = (targetIndex) => {
-    if (draggedImage) {
-      const updatedImages = [...images];
-
-      const removedImageIndex = updatedImages.findIndex(
-        (image) => image.id === draggedImage.id
-      );
-      if (removedImageIndex !== -1) {
-        updatedImages.splice(removedImageIndex, 1);
-        updatedImages.splice(targetIndex, 0, draggedImage);
-        setImages(updatedImages);
-        setDraggedImage(null);
-      }
-    }
-  };
-
-  const totalChecked = images.filter((image) => image.isChecked).length ?? 0;
+  const [draggedIndex, setDraggedIndex] = useState(null);
 
   const ImageCard = ({ elm, index }) => {
+
+    
+    const {
+      attributes,
+      listeners,
+      setNodeRef,
+      transform,
+      transition,
+    } = useSortable({ id: elm.id });
+    const style = {
+      transition,
+      transform: CSS.Transform.toString(transform),
+    };
     return (
       <div
-        draggable={true}
-        onDragStart={() => handleDragStart(elm)}
-        onDrop={() => handleDrop(index)}
-        className={`${index === 0 ? "row-span-2 col-span-2" : ""} group `}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+        // draggable={true}
+        // onDragStart={() => handleDragStart(elm)}
+        // onDrop={() => handleDrop(index)}
+        className={`${index === 0 ? "row-span-2 col-span-2" : " "} group `}
         key={index}
-        onClick={() => checked(index)}
-      >
+        onClick={() => {
+          console.log('Image clicked')
+          checked(index)}}      >
         <div className="relative overflow-hidden rounded-lg border-slate border-2  ">
           <img
             className="w-full"
             data-id={elm.id}
-            alt={elm.id}
+            
             src={elm?.image}
           />
           <div
@@ -75,16 +217,85 @@ function App() {
             <div className=" py-5 px-5">
               <input
                 className="w-5 h-5"
-                // onChange={() => checked(index)}
+                onClick={() => {
+                  console.log('Image clicked')
+                  checked(index)}}
                 checked={elm?.isChecked}
                 type="checkbox"
               ></input>
             </div>
           </div>
+
+          
         </div>
       </div>
     );
   };
+
+  const handleUnChecked = () => {
+    const uncheckedImages = images.map((img) => {
+      return { ...img, isChecked: false };
+    });
+    setImages(uncheckedImages);
+  };
+
+  const checked = (index) => {
+    console.log('checked')
+    const updatedImages = [...images];
+    updatedImages[index].isChecked = !updatedImages[index].isChecked;
+    setImages(updatedImages);
+  };
+
+  const handleDelete = () => {
+    console.log("delete");
+    const remainingImages = images.filter((image) => !image.isChecked);
+    setImages(remainingImages);
+  };
+
+
+  const onDragEnd = (event) => {
+    // console.log(event)
+    const { active, over } = event;
+    if (active.id === over.id) {
+      return;
+    }
+    setImages((images) => {
+      const oldIndex = images.findIndex((image) => image.id === active.id);
+      const newIndex = images.findIndex((image) => image.id === over.id);
+      return arrayMove(images, oldIndex, newIndex);
+    });
+  }
+
+  // const handleDragStart = (image) => {
+  //   setDragging(true);
+
+  //   setDraggedImage(image);
+  // };
+  // const handleDrop = (targetIndex) => {
+  //   setDragging(false);
+
+  //   if (draggedImage) {
+  //     const updatedImages = images.filter(
+  //       (image) => image.id !== draggedImage.id
+  //     );
+  //     updatedImages.splice(targetIndex, 0, draggedImage);
+
+  //     setImages(updatedImages);
+  //     setDraggedImage(null);
+  //   }
+  // };
+
+  // const handleDragOver = (e) => {
+  //   e.preventDefault();
+  //   const firstChild = e.target.children[0];
+  //   console.log("firstChild", firstChild);
+
+  //   if (firstChild && firstChild.dataset.id) {
+  //     setDraggedIndex(firstChild.dataset.id);
+  //   }
+  // };
+
+  const totalChecked = images.filter((image) => image.isChecked).length ?? 0;
 
   return (
     <>
@@ -105,7 +316,7 @@ function App() {
               </div>
 
               <div
-                className=" text-base md:text-2xl text-red-600 bg-slate-200 hover:bg-slate-400 rounded-md hover:cursor-pointer font-semibold px-2 md:px-4 py-1 md:py-2"
+                className=" text-base md:text-2xl  bg-slate-200 hover:bg-slate-400 rounded-md hover:cursor-pointer font-semibold px-2 md:px-4 py-1 md:py-2"
                 onClick={handleDelete}
               >
                 Delete
@@ -117,11 +328,12 @@ function App() {
             </div>
           )}
         </div>
+        
 
+        <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd} >
+          <SortableContext items={images} strategy={rectSortingStrategy}>
         <div
-          onDragOver={(e) => {
-            e.preventDefault();
-          }}
+          // onDragOver={handleDragOver}
           className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 pt-5 "
         >
           {images.length > 0 ? (
@@ -131,6 +343,7 @@ function App() {
           ) : (
             <p className="text-3xl font-bold">No Images </p>
           )}
+          
           <div className="flex flex-col w-full items-center justify-center bg-slate-100 border-dashed border-black border-2 rounded-lg">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -150,6 +363,8 @@ function App() {
             <p>Upload Images</p>
           </div>
         </div>
+        </SortableContext>
+        </DndContext>
       </div>
     </>
   );
